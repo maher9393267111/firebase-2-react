c32e16d54b03213738443a4056b0b22e06130ce1
maher <gomemahero.com>
2022-06-12 19:36:47 +0300

    CREATE COLLLECTION IN FIRESTORE AND ADD MY NAME AND INMAGE IUS WORK

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState, iseEffect } from "react";
-import { db, app, storage } from "./firebase/index";
-import { getStorage, ref, getDownloadURL, uploadBytes,listAll } from "firebase/storage";
+import { db, app, storage, } from "./firebase/index";
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytes,
+  listAll,
+} from "firebase/storage";
+import {setDoc,addDoc,collection} from "firebase/firestore";
 
 function App() {
- 
-     
-const [allimages, setAllimages] = useState([]);
-const [image, setImage] = useState(null);
-const [url, setUrl] = useState(null);
+  const [allimages, setAllimages] = useState([]);
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState(null);
 
-//send array of images to firebasestorage
-const sendImage = async(e) => {
+  //send array of images to firebasestorage
+  const sendImage = async (e) => {
+    e.preventDefault();
 
-  e.preventDefault();
+    let file = e.target.files[0];
+    // console.log(file);
 
-
-  let file = e.target.files[0];
-  // console.log(file);
-
-  setImage(URL.createObjectURL(file));
+    setImage(URL.createObjectURL(file));
 
 
-
-  
-   
- 
-
-
-
-}
+    let data = addDoc(collection(db, "mahercOLLECTION"), {
+      image: image,
+      name:'maher gome'
+    }).then(() => {
+      console.log("Document successfully written!");
+    }
+    ).catch((error) => {
+      console.error("Error writing document: ", error);
+    }
+    );
 
 
 
 
-
-
-
-
+  };
 
   return (
     <div className="container">
@@ -46,11 +48,7 @@ const sendImage = async(e) => {
       <h2>ADD PRODUCTS {}</h2>
       <hr />
 
-      <input type="file" multiple={true} 
-       onChange={sendImage}
-      
-     
-      />
+      <input type="file" multiple={true} onChange={sendImage} />
 
       <button
       // onClick={testDownloadImages}
