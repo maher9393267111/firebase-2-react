6852df449f82d850077ecb4ea4c8a1041eb2353e
maher <gomemahero.com>
2022-06-12 21:22:22 +0300

    sub-category created successfully start with  parentc ategory id send

--- src/components/childcategory.js
+++ src/components/childcategory.js
@@ -7,13 +7,21 @@ import {
   uploadBytes,
   listAll,
 } from "firebase/storage";
-import { setDoc, addDoc, collection, doc ,onSnapshot, updateDoc } from "firebase/firestore";
+import {
+  setDoc,
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { async } from "@firebase/util";
 const CreateCategory = () => {
   const [catinfo, setCatinfo] = useState("");
   const [catimage, setCatimage] = useState("");
   const [parentcat, setparentcat] = useState("");
+  const [selectedcat, setselectedcat] = useState("");
 
   const [allimages, setAllimages] = useState([]);
 
@@ -21,38 +29,29 @@ const CreateCategory = () => {
     setCatinfo(e.target.value);
   };
 
+  // ftch all parent categories
 
-// ftch all parent categories
-
-useEffect(() => {
+  useEffect(() => {
     // onSnapshot(doc(db, 'categories'), (doc) => {
     //   setparentcat(doc.data());
     // });
 
     const unsub = onSnapshot(collection(db, "categories"), (snapshot) => {
-        
-        let employeesArr = snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setparentcat(employeesArr);
-        }
-        );
-
-
- 
-
-
+      let employeesArr = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setparentcat(employeesArr);
+      console.log(employeesArr);
+    });
   }, []);
-
-
 
   const handleImageChange = async (e) => {
     const file = e.target.files[0];
     console.log(file);
 
     const storage = getStorage();
-    const testRef = ref(storage, `mycategories/${file.name}`);
+    const testRef = ref(storage, `childcategories/${file.name}`);
     console.log("testRef", testRef);
     // send image file to storage with test ref info
     await uploadBytes(testRef, file).then((snapshot) => {
@@ -67,9 +66,10 @@ useEffect(() => {
   const handlesubmit = (e) => {
     e.preventDefault();
 
-    let data = addDoc(collection(db, "categories"), {
-      catinfo: catinfo,
-      catimage: catimage,
+    let data = addDoc(collection(db, "childcategories"), {
+      childcat_name: catinfo,
+      childcat_image: catimage,
+        parentcat_name: selectedcat,
     });
   };
 
@@ -82,7 +82,7 @@ useEffect(() => {
               className="block text-gray-700 text-sm font-bold mb-2"
               for="username"
             >
-             Child-Category name {parentcat.length}
+              Child-Category name {selectedcat}
             </label>
             <input
               onChange={handleChange}
@@ -92,6 +92,26 @@ useEffect(() => {
               placeholder="Username"
             />
           </div>
+
+          {/* ---select parent cate---- */}
+
+          <div>
+            <div>
+              <select
+                onChange={(e) => {
+                  setselectedcat(e.target.value);
+                }}
+              >
+                {parentcat?.map((cat) => (
+                  <option value={cat?.id} key={cat?.id}>
+                    {" "}
+                    {cat?.catinfo}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
           <div class="mb-6">
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
