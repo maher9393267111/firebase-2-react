decf1320b84c30be4d3ea98c2efb19ab2bfbca9d
maher <gomemahero.com>
2022-06-12 14:03:48 +0300

    upload and send image to firebase storage start work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,iseEffect} from 'react';
+import {db,app} from './firebase/index';
+import {getStorage,ref,getDownloadURL,uploadBytes} from 'firebase/storage';
+
 
 function App() {
+
+  const [productName, setProductName] = useState('');
+  const [productPrice, setProductPrice] = useState(0);
+  const [productImg, setProductImg] = useState(null);
+  const [error, setError] = useState('');
+  const [imageUrl, setImageURL] = useState('');
+
+
+
+
+  const handleChangeImage = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    
+    const storage = getStorage();
+    const testRef = ref(storage, `myimages/${file.name}`);
+    console.log('testRef', testRef);
+    await uploadBytes(testRef, file).then((snapshot) => {
+      console.log('Uploaded image to storage success!');
+    });
+    const down = await getDownloadURL(testRef);
+    setImageURL(down); 
+   
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <div className='container'>
+    <br />
+    <h2>ADD PRODUCTS</h2>
+    <hr />
+  
+
+  <input type="file"  
+  onChange={handleChangeImage}
+  />
+
+
+<button
+// onClick={testDownloadImages}
+>upload image</button>
+
+<img
+className='  mx-auto  w-20 h-20'
+
+src={imageUrl} alt="" />
+
+
+
+</div>
+
+
+
+
   );
 }
 
