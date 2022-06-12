2c8eeda6818c10a8ea5a8ada3b51940d79b953ba
maher <gomemahero.com>
2022-06-12 19:29:08 +0300

    create url from image file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState,iseEffect} from 'react';
-import {db,app} from './firebase/index';
-import {getStorage,ref,getDownloadURL,uploadBytes} from 'firebase/storage';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, iseEffect } from "react";
+import { db, app, storage } from "./firebase/index";
+import { getStorage, ref, getDownloadURL, uploadBytes,listAll } from "firebase/storage";
 
 function App() {
+ 
+     
+const [allimages, setAllimages] = useState([]);
+const [image, setImage] = useState(null);
+const [url, setUrl] = useState(null);
 
-  const [productName, setProductName] = useState('');
-  const [productPrice, setProductPrice] = useState(0);
-  const [productImg, setProductImg] = useState(null);
-  const [error, setError] = useState('');
-  const [imageUrl, setImageURL] = useState('');
+//send array of images to firebasestorage
+const sendImage = async(e) => {
+
+  e.preventDefault();
+
+
+  let file = e.target.files[0];
+  // console.log(file);
+
+  setImage(URL.createObjectURL(file));
 
 
 
-
-  const handleChangeImage = async (e) => {
-    const file = e.target.files[0];
-    console.log(file);
-    
-    const storage = getStorage();
-    const testRef = ref(storage, `myimages/${file.name}`);
-    console.log('testRef', testRef);
-    await uploadBytes(testRef, file).then((snapshot) => {
-      console.log('Uploaded image to storage success!');
-    });
-    const down = await getDownloadURL(testRef);
-    setImageURL(down); 
+  
    
-  }
+ 
 
 
 
-
-
+}
 
 
 
@@ -45,34 +41,25 @@ function App() {
 
 
   return (
-  
-    <div className='container'>
-    <br />
-    <h2>ADD PRODUCTS</h2>
-    <hr />
-  
+    <div className="container">
+      <br />
+      <h2>ADD PRODUCTS {}</h2>
+      <hr />
 
-  <input type="file"  
-  onChange={handleChangeImage}
-  />
+      <input type="file" multiple={true} 
+       onChange={sendImage}
+      
+     
+      />
 
+      <button
+      // onClick={testDownloadImages}
+      >
+        upload image
+      </button>
 
-<button
-// onClick={testDownloadImages}
->upload image</button>
-
-<img
-className='  mx-auto  w-20 h-20'
-
-src={imageUrl} alt="" />
-
-
-
-</div>
-
-
-
-
+      <img className="  mx-auto  w-20 h-20" src={image} alt="" />
+    </div>
   );
 }
 
