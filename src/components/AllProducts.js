b29c57ff2fab8a54e2cfee599dc9d46462203e23
maher <gomemahero.com>
2022-06-13 05:21:38 +0300

    fetch single product by his id

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -11,7 +11,7 @@ import {
   onSnapshot,
   arrayUnion,
   arrayRemove,
-  getDoc
+  getDoc,
 } from "firebase/firestore";
 import { useAuth } from "../context";
 
@@ -19,8 +19,9 @@ const AllProducts = () => {
   const [products, setProducts] = useState([]);
   const [productanme, setProductname] = useState("");
   const [productImage, setProductImage] = useState("");
+  const [productbyid, setProductbyid] = useState("");
 
-  const {userinfo} = useAuth();
+  const { userinfo } = useAuth();
 
   useEffect(() => {
     const unsub = onSnapshot(collection(db, "products"), (snapshot) => {
@@ -33,95 +34,83 @@ const AllProducts = () => {
     });
   }, []);
 
- 
-
   const deletePro = (key) => {
-    console.log("Delete", key)
+    console.log("Delete", key);
     return new Promise(async (resolve, reject) => {
+      try {
+        await deleteDoc(doc(db, "products", key));
+        resolve();
+      } catch (error) {
+        console.log(error);
+        reject();
+      }
+    });
+  };
 
-        try {
-            await deleteDoc(doc(db, "products", key))
-            resolve()
-        } catch (error) {
-            console.log(error)
-            reject()
-        }
-    })
-}
+  // update------
 
-
-
-
-// update------
-
-
-const upatePro = (key) => {
-    console.log("Delete", key)
+  const upatePro = (key) => {
+    console.log("Delete", key);
     return new Promise(async (resolve, reject) => {
+      try {
+        await updateDoc(doc(db, "products", key), {
+          ...products[key],
+          product_name: "update",
+          product_price: 366,
+        });
+        resolve();
+      } catch (error) {
+        console.log(error);
+        reject();
+      }
+    });
+  };
 
-        try {
-            await updateDoc(doc(db, "products", key), {
-              ...products[key],
-                product_name: "update",
-                product_price: 366,
-            })
-            resolve()
-        } catch (error) {
-            console.log(error)
-            reject()
-        }
-    })
-}
+  // update userinfo cart
 
-
-
-
-// update userinfo cart
-
-const addtocart = (key) => {
-
-  return new Promise(async (resolve, reject) => {
-
+  const addtocart = (key) => {
+    return new Promise(async (resolve, reject) => {
       try {
 
-await getDoc(doc(db, "users", userinfo.email)).then((res) => {
-  
-  console.log(res.data().cart)
-  const cartdata = res.data().cart
+// finf oduct by key from products database
 
-  // check if exist in cart remove it
-  if (cartdata.includes(key)) {
-    console.log("exist")
-    const newcart = cartdata.filter((item) => item !== key)
-    updateDoc(doc(db, "users", userinfo.email), {
-      cart: newcart,
-    })
-  } else {
-    console.log("not exist")
-
-
-         updateDoc(doc(db, "users", userinfo.email), {
-            ...userinfo,
-            cart: arrayUnion(key),
-            
-           
-          })
+        const product = await getDoc(doc(db, "products", key)).then((doc) => {
+          doc.data();
+          console.log('productbyid------>',doc.data());
         }
-      }
-    )
-          console.log("add to cart success")
-          resolve()
+        );
+        
+
+
+
+        await getDoc(doc(db, "users", userinfo.email)).then((res) => {
+          console.log(res.data().cart);
+          const cartdata = res.data().cart;
+
+          // check if exist in cart remove it
+          if (cartdata.includes(key)) {
+            console.log("exist");
+            const newcart = cartdata.filter((item) => item !== key);
+            updateDoc(doc(db, "users", userinfo.email), {
+              cart: newcart,
+            });
+          } else {
+            console.log("not exist");
+
+            updateDoc(doc(db, "users", userinfo.email), {
+              ...userinfo,
+              cart: arrayUnion(key),
+            });
+          }
+        });
+        console.log("add to cart success");
+        resolve();
       } catch (error) {
-console.log(error)
-          reject()
+        console.log(error);
+        reject();
       }
-  }
-  )
-}
-
-
-
-
+    });
+  };
 
   return (
     <div>
@@ -149,9 +138,11 @@ border-2 border-gray-500  shadow-lg  pb-12 pl-6 pr-6"
 
                 <div className=" absolute  text-sm top-[-10px] right-[-9px]   font-bold rounded-full  ">
                   <img
-                  onClick={() => addtocart(product.id)}
-                  className="w-8 h-8 rounded-full  object-cover"
-                  src="https://cdn2.iconfinder.com/data/icons/basic-flat-icon-set/128/bag-256.png" alt="" />
+                    onClick={() => addtocart(product.id)}
+                    className="w-8 h-8 rounded-full  object-cover"
+                    src="https://cdn2.iconfinder.com/data/icons/basic-flat-icon-set/128/bag-256.png"
+                    alt=""
+                  />
                 </div>
               </div>
 
