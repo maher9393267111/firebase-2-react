acf884b138b1b8f620c0b7059efa315742a81612
maher <gomemahero.com>
2022-06-14 16:39:43 +0300

    zustand is work when  and states not remobve when refresh the poage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useStore } from '../redux/zutstore';
 // import CreateCategory from '../components/createcategory';
 // import Childcategory from '../components/childcategory';
 // import CreateProduct from '../components/createProduct';
@@ -14,14 +15,33 @@ import {
 import { setDoc, addDoc, collection, doc } from "firebase/firestore";
 
 const Home = () => {
+    const title = useStore(state => state.formState.title);
+
+    const setTitle = useStore(state => state.setTitle);
+
+
+
+
     return (
         <div>
             <h1>Home Pages</h1>
 
 
+
+            {title}
+
+<input
+    className="w-80  border-2 border-Blue-500 focus:border-Teal-600 text-midnight text-[#18181b] font-serif text-base indent-1  placeholder:text-[#9ca3af] placeholder:text-center italic outline-none"
+    type="text"
+    placeholder="Ne yapılması gerekiyor ? "
+    required
+    value={title}
+    onChange={setTitle}
+  />
+
 <div>
 
-<AllProducts/>
+
 
 </div>
 
